693af9295b03ffdec9b86afb8e6b2d74d80b03f0
Ryan Kagrise <rdkcode@gmail.com>
2022-06-20 19:31:33 -1000

    added first python solution

--- arrays/twoNumSum/twoNumSum.py
+++ arrays/twoNumSum/twoNumSum.py
@@ -12,3 +12,17 @@
 
 ##You can assume that there will be at most one pair of numbers summing up to the target sum.
 
+
+
+##Potential Solution A
+
+def two_number_sum_a(array, target_sum):
+    for i in range(len(array) - 1):
+        first_num = array[i]
+        for j in range(i + 1, len(array)):
+            second_num = array[j]
+            if first_num + second_num == target_sum:
+                return [first_num, second_num]
+    return []
+
+## Time & Space Complexity: Time = O(n^2) & Space = O(1)
