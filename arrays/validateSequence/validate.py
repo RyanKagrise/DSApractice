e132810f2cf67cfb31dc9bccefe61b1ca0e001b0
Ryan Kagrise <rdkcode@gmail.com>
2022-06-27 15:09:44 -1000

    added validate python

--- arrays/validateSequence/validate.py
+++ arrays/validateSequence/validate.py
@@ -5,3 +5,21 @@
 # For instance, the numbers [1,3,4] form a subsequence of the array [1,2,3,4], and so do the numbers [2,4]. Note that a single number in an array and the array itself are both valid subsequences of the array.
 
 
+def valid_sequence(array, sequence) {
+    arrayIndex = 0
+    sequenceIndex = 0
+    while arrayIndex < len(array) and sequenceIndex < len(sequence):
+      if array[arrayIndex] == sequence[sequenceIndex]:
+          sequenceIndex += 1
+      arrayIndex += 1
+    return sequenceIndex == len(sequence)
+}
+
+
+# time and space = O(n) time and O(1) space
+
+
+
+# alternative solution
+
+
