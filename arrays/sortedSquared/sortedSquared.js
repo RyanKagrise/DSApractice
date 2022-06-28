5383839885181810aa94a70420dae4e1fa10e79c
Ryan Kagrise <rdkcode@gmail.com>
2022-06-27 15:01:10 -1000

    working on other problems

--- arrays/sortedSquared/sortedSquared.js
+++ arrays/sortedSquared/sortedSquared.js
@@ -1,4 +1,5 @@
 // Write a function that takes in a non-empty array of integers that are sorted in an ascending order and returns a new array of the same length with the squares
+
 // of the original integers also sorted in ascending order.
 
 function sortedSquared (array) {
---
+++ arrays/sortedSquared/sorted_squared.py
@@ -0,0 +1,3 @@
+# Write a function that takes in a non-empty array of integers that are sorted in an ascending order and returns a new array of the same length with the squares
+
+# of the original integers also sorted in ascending order.
