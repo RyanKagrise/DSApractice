5b38f1f72e61efb602f4b30dd43e4ba9b2147e4f
Ryan Kagrise <rdkcode@gmail.com>
2022-06-28 18:40:45 -1000

    scratch cont

--- arrays/twoNumSum/scratch.js
+++ arrays/twoNumSum/scratch.js
@@ -10,3 +10,18 @@ function twoNumSum (array, targetSum) {
   }
   return []
 }
+
+
+
+function twoNumSumB (array, targetSum) {
+  const nums = {};
+  for (const num of array) {
+    const potentialMatch = targetSum - num;
+    if (potentialMatch in nums) {
+      return [potentialMatch, num];
+    } else {
+      nums[num] = true;
+    }
+  }
+  return []
+}
