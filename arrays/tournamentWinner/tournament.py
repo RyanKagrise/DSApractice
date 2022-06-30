8c1d7076549d426839d5f742c105da627aef5957
Ryan Kagrise <rdkcode@gmail.com>
2022-06-29 15:59:12 -1000

    added explanations on py

--- arrays/tournamentWinner/tournament.py
+++ arrays/tournamentWinner/tournament.py
@@ -3,28 +3,28 @@
 
 def tournamentWinner(comp, results):
 
-dic = {}
-max = 0
-current_winner = '#'
+dic = {} #dictionary to store winners
+max = 0 #variable to track min points of any team
+current_winner = '#' #variable to track current winner
 
-for i in range(len(comp)):
+for i in range(len(comp)): #iterate through comp list
 
   match = comp[i]
-  resultIndex = abs(results[i] - 1)
+  resultIndex = abs(results[i] - 1) #find index of winner from result list
   winner = comp[i][resultIndex]
 
   if winner in dic:
       pts = dic[winner]
       pts += 3
-      if pts > max:
+      if pts > max: #check to see if update pts is greater than current maximum
           max = pts
           current_winner = winner
-      dic.update({winner : pts})
+      dic.update({winner : pts}) #update winner record
 
   else:
       pts = 3
       dic[winner] = 3
-      if pts > max:
+      if pts > max: #check to see if winner pts is greater than current maximum
           max = pts
           current_winner = winner
 
