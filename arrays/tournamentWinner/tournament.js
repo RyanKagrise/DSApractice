df07a1883c12101f6c5897953b55117a9402e17d
Ryan Kagrise <rdkcode@gmail.com>
2022-06-29 15:50:55 -1000

    added helper to tournament

--- arrays/tournamentWinner/tournament.js
+++ arrays/tournamentWinner/tournament.js
@@ -1,18 +1,26 @@
-const home_team_win = 1;
+const homeTeamWin = 1;
 
 function tournamentWinner (competitions, results) {
-  let current_best_team = '';
-  const scores = {[current_best_team]: 0};
+  let currentBestTeam = '';
+  const scores = {[currentBestTeam]: 0};
 
   for (let i = 0; i < competitions.length; i++) {
     const result = results[i];
     const [homeTeam, awayTeam] = competitions[i];
 
-    const winningTeam = result === home_team_win ? homeTeam : awayTeam;
+    const winningTeam = result === homeTeamWin ? homeTeam : awayTeam;
 
-    if (scores[winningTeam] > scores[current_best_team]) {
-      current_best_team = winningTeam;
+    updateScores(winningTeam, 3, scores);
+
+    if (scores[winningTeam] > scores[currentBestTeam]) {
+      currentBestTeam = winningTeam;
     }
   }
-  return current_best_team;
+  return currentBestTeam;
+}
+
+function updateScores(team, points, scores) {
+  if (!(team in scores)) scores[team] = 0;
+
+  scores[team] += points;
 }
