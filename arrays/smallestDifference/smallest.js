4ae231e3458b760ffdbaa0eea5ea97f864ccdca8
Ryan Kagrise <rdkcode@gmail.com>
2022-07-20 10:14:48 -1000

    smallest js working

---
+++ arrays/smallestDifference/smallest.js
@@ -0,0 +1,12 @@
+function smallestDifference(arrayOne, arrayTwo) {
+  arrayOne.sort((a,b) => a - b);
+  arrayTwo.sort((a,b) => a - b);
+  let indexOne = 0;
+  let indexTwo = 0;
+  let smallest = Infinity;
+  let current = Infinity;
+  let smallestPair = [];
+  while (indexOne < arrayOne.length && indexTwo < arrayTwo.length) {
+    
+  }
+}
