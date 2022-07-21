2141b9b0fa8d41ac5b351ae3c4e17c25ad868082
Ryan Kagrise <rdkcode@gmail.com>
2022-07-20 17:35:51 -1000

    bubble still needs helper

--- sort/bubble/bubble.js
+++ sort/bubble/bubble.js
@@ -3,3 +3,13 @@ function bubbleSort(array) {
   let counter = 0;
   while (!isSorted) {
     isSorted = true;
+    for (let i = 0; i < array.length - 1 - counter; i++) {
+      if (array[i] > array[i + 1]) {
+        swap(i, i + 1, array);
+        isSorted = false;
+      }
+    }
+    counter++;
+  }
+  return array;
+}
