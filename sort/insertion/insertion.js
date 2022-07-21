3374bec4130a495edab8157cb21ed4b9954ddb41
Ryan Kagrise <rdkcode@gmail.com>
2022-07-21 10:40:18 -1000

    minor bug fix

--- sort/insertion/insertion.js
+++ sort/insertion/insertion.js
@@ -1,5 +1,5 @@
 function insertionSort(array) {
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 1; i < array.length; i++) {
     let j = i;
     while (j > 0 && array[j] < array[j - 1]) {
       swap(j, j - 1, array);
--- sort/insertion/insertion.py
+++ sort/insertion/insertion.py
@@ -1,6 +1,6 @@
 def insertion_sort(array):
   for i in range(1, len(array)):
-      j = 1
+      j = i
       while j > 0 and array[j] < array[j-1]:
         swap(j, j - 1, array)
         j -= 1
