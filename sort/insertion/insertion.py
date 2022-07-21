7f4ca7a74bc8e019881661e997e408e66f9f0c2d
Ryan Kagrise <rdkcode@gmail.com>
2022-07-21 10:32:04 -1000

    added insertion py

---
+++ sort/insertion/insertion.py
@@ -0,0 +1,10 @@
+def insertion_sort(array):
+  for i in range(1, len(array)):
+      j = 1
+      while j > 0 and array[j] < array[j-1]:
+        swap(j, j - 1, array)
+        j -= 1
+  return array
+
+def swap (i, j, array):
+  array[i], array[j] = array[j], array[i]
