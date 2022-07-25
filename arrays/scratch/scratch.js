e2780bbeea11432fbe166c8ca80397f1724c0765
Ryan Kagrise <rdkcode@gmail.com>
2022-07-24 15:38:22 -1000

    stop scratching bruh

--- arrays/scratch/scratch.js
+++ arrays/scratch/scratch.js
@@ -57,3 +57,21 @@ function twoNumAgain(array, targetSum) {
 
 
 //time and space = o(n^2) time and o(1) space;
+
+
+function twoNumAgain (array, targetSum) {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  while (left < right) {
+    const currentSum = array[left] + array[right];
+    if (currentSum === targetSum) {
+      return [array[left], array[right]];
+    } else if (currentSum < targetSum) {
+      left++;
+    } else if (currentSum > targetSum) {
+      right--;
+    }
+  }
+  return [];
+}
