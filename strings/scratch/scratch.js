795d3a705162066560877a92828794e714df99cc
Ryan Kagrise <rdkcode@gmail.com>
2022-07-28 10:25:11 -1000

    scratching strings

--- strings/scratch/scratch.js
+++ strings/scratch/scratch.js
@@ -11,3 +11,19 @@ function stringChecker(string) {
 }
 
 //time and space = o(nlog(n)) time and o(1) space
+
+
+function alphabetSwitcher(string, key) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const newLetters = [];
+  const newKey = key % 26;
+  for (const letter of string) {
+    newLetters.push(getNewLetter(letter, key, alphabet));
+  }
+  return newLetters.join('');
+}
+
+function getNewLetter(letter, key, alphabet) {
+  const newLetterCode = alphabet.indexOf(letter) + key;
+  return alphabet[newLetterCode % 26];
+}
