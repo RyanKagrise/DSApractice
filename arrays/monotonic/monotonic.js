81d39f1415e7885f9c5e3240aee7066928d4a259
Ryan Kagrise <rdkcode@gmail.com>
2022-07-30 16:54:53 -1000

    monotonic

---
+++ arrays/monotonic/monotonic.js
@@ -0,0 +1,13 @@
+function isMonotonic(array) {
+  // Write your code here.
+  let isNonDecreasing = true;
+  let isNonIncreasing = true;
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < array[i - 1]) isNonDecreasing = false;
+    if (array[i] > array[i - 1]) isNonIncreasing = false;
+  }
+  return isNonDecreasing || isNonIncreasing;
+}
+
+// Do not edit the line below.
+exports.isMonotonic = isMonotonic;
