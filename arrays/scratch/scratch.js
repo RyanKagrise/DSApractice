8e7ab76e82b179d01f68579367d866fb817c867b
Ryan Kagrise <rdkcode@gmail.com>
2022-08-01 19:15:27 -1000

    working little scratch

--- arrays/scratch/scratch.js
+++ arrays/scratch/scratch.js
@@ -235,3 +235,40 @@ function sortedSquare(array) {
 
 
 //time and space = o(n) time and o(n) space
+
+
+function twoNumberSum(array, targetSum) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let firstNum = array[i];
+    for (let j = i + 1; i < array.length; j++) {
+      let secondNum = array[j];
+      if (firstNum + secondNum === targetSum) {
+        return [firstNum, secondNum]
+      }
+    }
+  }
+  return []
+}
+
+// time and space = o(n^2) time and o(1) space;
+
+
+
+//--------------------------------------------------------------
+
+
+function isValidSequence(array, sequence) {
+  let arrayIndex = 0;
+  let sequenceIndex = 0;
+  while (arrayIndex < array.length && sequenceIndex < sequence.length) {
+    if (array[arrayIndex] === sequence[sequenceIndex]) sequenceIndex;
+    arrayIndex++;
+  }
+  return sequenceIndex === sequence.length;
+}
+
+//time and space= o(n) time and o(1) space;
+
+
+
+//--------------------------------------------------------------
