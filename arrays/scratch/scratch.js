dedf2dbb5f27574b2e265dc04a93d446603f91df
Ryan Kagrise <rdkcode@gmail.com>
2022-08-02 20:23:19 -1000

    limited scratch

--- arrays/scratch/scratch.js
+++ arrays/scratch/scratch.js
@@ -272,3 +272,20 @@ function isValidSequence(array, sequence) {
 
 
 //--------------------------------------------------------------
+
+function validationAgain(array, sequence) {
+  let arrayIndex = 0;
+  let sequenceIndex = 0;
+  while (arrayIndex < array.length && sequenceIndex < sequence.length) {
+    if (array[arrayIndex] === sequence[sequenceIndex]) sequenceIndex++;
+    arrayIndex++;
+  }
+  return sequenceIndex === sequence.length;
+}
+
+
+//time and space = o(n) time and o(1) space
+
+
+
+//--------------------------------------------------------------
