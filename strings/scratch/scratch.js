0071d53a2e611d55a7a50b03844ce721fc66728e
Ryan Kagrise <rdkcode@gmail.com>
2022-08-06 00:12:38 -1000

    daily

--- strings/scratch/scratch.js
+++ strings/scratch/scratch.js
@@ -71,6 +71,43 @@ function getNewLetter(letter, key, alphabet) {
 
 //--------------------------------------------------------------
 
+
+function stringChecker(string) {
+  return string === string.split('').reverse().join('');
+}
+
+//time and space = o(nlog(n)) time and o(1) space
+
+
+
+
+
+
+//--------------------------------------------------------------
+
+
+function alphabetSwitcher(string, key) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const newLetters = [];
+  const newKey = key % 26;
+  for (const letter of string) {
+    newLetters.push(getNewLetter(letter, key, alphabet));
+  }
+  return newLetters.join('');
+}
+
+function getNewLetter(letter, key, alphabet) {
+  const newLetterCode = alphabet.indexOf(letter) + key;
+  return alphabet[newLetterCode % 26];
+}
+
+//time and space = o(n) time and o(n) space
+
+
+
+
+//----------------------------------------------------------
+
 //--------------------------------------------------------------
 
 //--------------------------------------------------------------
