682f0b5f276e5f1343ad95873d8b357bce603e5d
Ryan Kagrise <rdkcode@gmail.com>
2022-08-08 11:51:06 -1000

    added products array

---
+++ arrays/arrayOfProducts/products.js
@@ -0,0 +1,14 @@
+function productsArray(array) {
+  const products = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let runningProduct = 1;
+    for (let j = 0; j < array.length; j++) {
+      if (i !== j) {
+        runningProduct *= array[j];
+      }
+      products[i] = runningProduct;
+    }
+  }
+  return products;
+}
