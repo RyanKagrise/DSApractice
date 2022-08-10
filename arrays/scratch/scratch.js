bdafbe4a1933997fd40818b2ca196914da5336cf
Ryan Kagrise <rdkcode@gmail.com>
2022-08-09 17:55:32 -1000

    daily practice

--- arrays/scratch/scratch.js
+++ arrays/scratch/scratch.js
@@ -383,3 +383,21 @@ function sortedSquared (array) {
   return sortedSquares;
 
 }
+
+//------------------------------------------------
+
+
+function productsArrayScratcher(array) {
+  const products = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let runningProduct = 1;
+    for (let j = 0; j < array.length; j++) {
+      if (i !== j) {
+        runningProduct *= array[j];
+      }
+      products[i] = runningProduct;
+    }
+  }
+  return products;
+}
