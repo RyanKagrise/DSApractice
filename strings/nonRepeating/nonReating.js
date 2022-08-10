d48b3aca152e42b1b0725890f334fcfa94d0009a
Ryan Kagrise <rdkcode@gmail.com>
2022-08-10 08:15:25 -1000

    nonRepeating

---
+++ strings/nonRepeating/nonReating.js
@@ -0,0 +1,16 @@
+function firstNonRepeatingCharacter(string) {
+  const characterFrequency = {};
+
+  for (const character of string) {
+    if (!(character in characterFrequency)) characterFrequency[character] = 0;
+    characterFrequency[character]++;
+  }
+
+  for (let i = 0; i < string.lenght; i++) {
+    const character = string[i];
+    if (characterFrequency[character] === 1) return i;
+  }
+
+  return -1;
+
+}
