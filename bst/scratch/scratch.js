e399503100e891fc385fbcfab28f378691079a81
Ryan Kagrise <rdkcode@gmail.com>
2022-08-12 19:27:58 -1000

    daily

---
+++ bst/scratch/scratch.js
@@ -0,0 +1,19 @@
+function dfs (tree, target, diff) {
+  if (!tree) return;
+
+  const currentDiff = Math.abs(target - tree.value);
+  if (currentDiff < diff.value) {
+    diff.closest = tree.value;
+    diff.value = currentDiff;
+  }
+
+  if (target < tree.value) dfs(tree.left, target, diff);
+  else if (target > tree.value) dfs(tree.right, target, diff);
+}
+
+function findClosestValueInBst(tree, target) {
+  const diff = { value: Infinity, closest: null }
+  dfs(tree, target, diff);
+  return diff.closest;
+}
+
