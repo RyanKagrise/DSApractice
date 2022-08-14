9a89dd4477b710826f1ee4b40e70e83aa44e5071
Ryan Kagrise <rdkcode@gmail.com>
2022-08-13 20:52:10 -1000

    daily practice

--- arrays/scratch/scratch.js
+++ arrays/scratch/scratch.js
@@ -411,3 +411,14 @@ function productsArrayScratcher(array) {
 //-------------------------------------------
 
 
+function validation(array, sequence) {
+  let arrayIndex = 0;
+  let sequenceIndex = 0;
+  while (arrayIndex < array.length && sequenceIndex < sequence.length) {
+    if (array[arrayIndex] === sequence[sequenceIndex]) sequenceIndex++;
+    arrayIndex++;
+  }
+  return sequenceIndex === sequence.length;
+}
+
+
