df34138ced94f760731d4234df8e22a1206fcfbe
Ryan Kagrise <rdkcode@gmail.com>
2022-08-15 20:43:12 -1000

    working it

--- arrays/scratch/scratch.js
+++ arrays/scratch/scratch.js
@@ -437,3 +437,22 @@ function sortedSquare(array) {
   return squaredArray;
 }
 
+//-----------------------
+
+
+function dailyPractice(array, targetSum) {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  while (left < right) {
+    let currentSum = array[left] + array[right];
+    if (currentSum === targetSum) {
+      return [array[left], array[right]];
+    } else if (currentSum < targetSum) {
+      left++;
+    } else if (currentSum > targetSum) {
+      right--;
+    }
+  }
+  return [];
+}
