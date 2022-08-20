dab01a6c6edcdda8b694594ec08f1296f56ab097
Ryan Kagrise <rdkcode@gmail.com>
2022-08-19 17:07:45 -1000

    daily rep

--- arrays/scratch/scratch.js
+++ arrays/scratch/scratch.js
@@ -480,3 +480,24 @@ function practice(array, targetSum) {
 // time and space = o(n) time and o(n) space
 
 
+//--------------------------------
+
+function practicing(array, targetSum) {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  while (left < right) {
+    let currentSum = array[left] + array[right];
+    if (currentSum === targetSum) {
+      return [array[left], array[right]];
+    } else if (currentSum < targetSum) {
+      left++;
+    } else if (currentSum > targetSum) {
+      right--;
+    }
+  }
+  return [];
+}
+
+
+//time and space = o(nlogn) time and o(1) space
