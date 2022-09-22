5a2253180584ae9b047bd32c32451ad45e50c120
Ryan Kagrise <rdkcode@gmail.com>
2022-09-21 18:02:42 -1000

    working it oldschool

--- arrays/scratch/scratch.js
+++ arrays/scratch/scratch.js
@@ -505,3 +505,19 @@ function practicing(array, targetSum) {
 
 //----------------------------------------------
 
+function longTime(array, targetSum) {
+  array.sort((a, b) => a - b);
+  let left = 0;
+  let right = array.length - 1;
+  while(left < right) {
+    let currentSum = array[left] + array[right];
+    if (currentSum === targetSum) {
+      return [array[left],  array[right]];
+    } else if (currentSum < targetSum) {
+      left++;
+    } else if (currentSum > targetSum) {
+      right--;
+    }
+  }
+  return [];
+}
