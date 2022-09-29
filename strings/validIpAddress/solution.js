07135d7c4c21069ddf88bfe0859e81ab746f894e
Ryan Kagrise <rdkcode@gmail.com>
2022-09-28 15:37:54 -1000

    fixed final solution

--- strings/validIpAddress/solution.js
+++ strings/validIpAddress/solution.js
@@ -1,35 +1,35 @@
-function validIpAddress(string) {
-  const ipAddressFound = [];
+function validIPAddresses(string) {
+  const ipAddressesFound = [];
 
   for (let i = 0; i < Math.min(string.length, 4); i++) {
-    const currentIpAddressParts = ['','','',''];
+    const currentIPAddressParts = ['','','',''];
 
-    currentIpAddressParts[0] = string.slice(0, i);
-    if (!isValidPart(currentIpAddressParts[0])) continue;
+    currentIPAddressParts[0] = string.slice(0, i);
+    if (!isValidPart(currentIPAddressParts[0])) continue;
 
     for (let j = i + 1; j < i + Math.min(string.length - i, 4); j++) {
-      currentIpAddressParts[1] = string.slice(i, j);
-      if (!isValidPart(currentIpAddressParts[1])) continue;
+      currentIPAddressParts[1] = string.slice(i, j);
+      if (!isValidPart(currentIPAddressParts[1])) continue;
 
-      for (let k = j + 1; k< j + Math.min(string.length - j, 4); k++) {
-        currentIpAddressParts[2] = string.slice(j, k);
-        currentIpAddressParts[3] = string.slice(k);
+      for (let k = j + 1; k < j + Math.min(string.length - j, 4); k++) {
+        currentIPAddressParts[2] = string.slice(j, k);
+        currentIPAddressParts[3] = string.slice(k);
 
-        if (isValidPart(currentIpAddressParts[2]) && isValidPart(currentIpAddressParts[3])) {
-          ipAddressFound.push(currentIpAddressParts.join('.'));
+        if (isValidPart(currentIPAddressParts[2]) && isValidPart(currentIPAddressParts[3])) {
+          ipAddressesFound.push(currentIPAddressParts.join('.'));
         }
       }
     }
   }
-  return ipAddressFound;
+  return ipAddressesFound;
 }
 
 
 function isValidPart(string) {
-  const stringAsInteger = parseInt(string);
+  const stringAsInt = parseInt(string);
   if (stringAsInt > 255) return false;
 
-  return string.length === stringAsInteger.toString().length;
+  return string.length === stringAsInt.toString().length;
 }
 
 
