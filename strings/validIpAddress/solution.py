b8205ca81e56cbd42f716376835cb258979c7063
Ryan Kagrise <rdkcode@gmail.com>
2022-09-28 15:41:38 -1000

    added helper

--- strings/validIpAddress/solution.py
+++ strings/validIpAddress/solution.py
@@ -2,9 +2,14 @@ def validIPAddresses(string) {
 
 }
 
-def isValidPart(string) {
 
-}
+def isValidPart(string):
+  string_as_int = int(string)
+  if string_as_int > 255:
+    return False
+
+  return len(string) == len(str(string_as_int))
+
 
 
 ##time and space:
